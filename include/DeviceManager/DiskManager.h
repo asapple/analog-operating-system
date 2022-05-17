5a9603cb2fc85a279d13add913fef31ae65befca
Arete <KakiaArete0@163.com>
2022-05-18 01:46:13 +0800

    Partial realization of disk management interface

--- include/DeviceManager/DiskManager.h
+++ include/DeviceManager/DiskManager.h
@@ -2,15 +2,22 @@
 #define DISKMANAGER_H
 #include "include/Common/Common.h"
 #include <QVector>
-namespace os {
-    class DiskManager {
-    public:
-        // parameter: blocks to be occupied
-        // return: occupied blocks id
-        QVector<int> RequestDisk(int blocks_need);
+#include <QBitArray>
 
-        // parameter: reverse procedure from RequestDisk()
-        int ReleaseDisk(QVector<int> blocks_vector);
+namespace os
+{
+    class DiskManager
+    {
+        QBitArray disk_bitmap; //表示磁盘的位图
+
+        DiskManager();
+        ~DiskManager(){};
+
+    public:
+        QVector<int> RequestDisk(int blocks_need);   //申请磁盘资源 ，返回占用的磁盘块的列表
+        int ReleaseDisk(QVector<int> blocks_vector); // 释放磁盘资源
+
+        QVector<int> GetDisk(); //获取整个磁盘的占用情况
     };
 }
 #endif // DISKMANAGER_H
