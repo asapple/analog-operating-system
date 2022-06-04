b47fb1a7fd79486542f506a72a9f266b1bbbd4f2
彭笑凯 <58331511+XPeng-max@users.noreply.github.com>
2022-06-04 23:06:09 +0800

    加一点注释

--- UI/form/ProcView.cpp
+++ UI/form/ProcView.cpp
@@ -19,14 +19,17 @@ namespace ui {
     };
 }
 static void updateProcessList(QTableWidget* table, int rowno, PCB& pcb);
-
+/**
+ * @brief frmMain::updateProcView
+ * 获取当前的进程状态数据，并用最新数据进程界面视图
+ */
 void frmMain::updateProcView() {
     auto& pm = ProcessManager::Instance();
     pid_t run = pm.GetRunProcess();
     auto waitList = pm.GetWaitQueue();
     auto readyList = pm.GetReadyQueue();
 
-    // set running
+    // 运行程序
     PCB pcb = pm.GetPCB(run);
     ui->curTime_value->setText(QString::number(pm.GetCurTime()));
     if (run != 0) {
@@ -44,22 +47,29 @@ void frmMain::updateProcView() {
         ui->IR_value->clear();
         ui->Pri_value->clear();
     }
+    // 获取当前就绪队列数据并更新到视图上
+    // 清空就绪队列的视图数据
     int rowCount = ui->ready_table->rowCount();
     for(int i = 0; i < rowCount; ++i)
     {
          ui->ready_table->removeRow(0);
     }
     ui->ready_table->clearContents();
+    // 插入就绪队列的数据
     for (int i = 0; i < readyList.size(); i++) {
         ui->ready_table->insertRow(i);
         updateProcessList(ui->ready_table, i, pm.GetPCB(readyList[i]));
     }
+
+    // 获取当前等待队列数据并更新到视图上
+    // 清空等待队列的视图数据
     rowCount = ui->block_table->rowCount();
     for(int i = 0; i < rowCount; ++i)
     {
          ui->block_table->removeRow(0);
     }
     ui->block_table->clearContents();
+    // 插入等待队列的数据
     for (int i = 0; i < waitList.size(); i++) {
         ui->block_table->insertRow(i);
         updateProcessList(ui->block_table, i, pm.GetPCB(waitList[i]));
